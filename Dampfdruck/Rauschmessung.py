e80300ad3c60b9f64816a07909db9d942726ad5d
lc316353 <44553345+lc316353@users.noreply.github.com>
2018-10-29 20:17:34 +0100

    Update Rauschmessung.py

--- Dampfdruck/Rauschmessung.py
+++ Dampfdruck/Rauschmessung.py
@@ -18,10 +18,10 @@ data = cassy.CassyDaten("Rauschmessung.lab")
 t = data.messung(1).datenreihe("t").werte
 T = data.messung(1).datenreihe("&J_A11").werte
 p = data.messung(1).datenreihe("p_B1").werte
-    
+
 print("Temperaturverteilung") #vllt Poisson???
-print("s=" + str(np.std(T))+", m="+str(np.average(T))+", err="+str(np.std(T)/np.sqrt(len(T))))
-plt.hist(T, normed=True)
+print("m=" + str(np.average(T))+", s="+str(np.std(T))+", err="+str(np.std(T)/np.sqrt(len(T))))
+plt.hist(T,7, normed=True)
 x=np.arange(min(T), max(T), 0.001)
 plt.plot(x, gauss(x, np.average(T), np.std(T)))
 plt.title(u"Temperaturverteilung+Gauß\n s=" + str(np.std(T))+", m="+str(np.average(T)))
@@ -33,15 +33,16 @@ plt.plot(t, T)
 plt.title("Rauschmessung Raumtemperatur")
 plt.ylabel(u"T/°C")
 plt.xlabel('time/s')
+plt.plot(t,np.array([np.mean(T)]*len(t)),color='green')
 plt.figure()
 
 print("==="*20)
 print("Druckverteilung") #vllt Poisson???
-print("s=" + str(np.std(p))+", m="+str(np.average(p))+", err="+str(np.std(p)/np.sqrt(len(p))))
+print("m=" + str(np.average(p))+", s="+str(np.std(p))+", err="+str(np.std(p)/np.sqrt(len(p))))
 
 x=np.arange(min(p), max(p), 0.01)
 plt.plot(x, gauss(x, np.average(p), np.std(p)))
-plt.hist(p, normed=True, bins=np.arange(984, 986, 0.25))
+plt.hist(p, 3, normed=True)#, bins=np.arange(984, 986, 0.25))
 plt.title(u"Druckverteilung+Gauß \n s=" + str(np.std(p))+", m="+str(np.average(p)))
 plt.xlabel(u"p/hPa")
 plt.ylabel('rel')
@@ -51,3 +52,4 @@ plt.plot(t, p)
 plt.ylabel(u"p/hPa")
 plt.xlabel('time/s')
 plt.title("Rauschmessung Atmosphärendruck")
+plt.plot(t,np.array([np.mean(p)]*len(t)),color='green')
