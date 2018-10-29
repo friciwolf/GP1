4b7e22ba02d196e29a91564d1aceb968367bb623
Mate <friciwolf@gmail.com>
2018-10-30 00:51:20 +0100

    Update Dampfdruck/Rauschmessung.py

--- Dampfdruck/Rauschmessung.py
+++ Dampfdruck/Rauschmessung.py
@@ -13,6 +13,10 @@ import praktikum.cassy as cassy
 def gauss(x, m, s):
     return(np.power(scipy.pi*2*s**2, -0.5)*np.exp(-(x-m)**2/(2*s**2)))
 
+def round_up(x, digits): #obere Abschätzung der Fehler
+    if digits!=0: return np.ceil(x*10**digits)/10**digits
+    else: return np.ceil(x)
+
 t, p, T = [], [], [] #Temperatur in °C!
 data = cassy.CassyDaten("Rauschmessung.lab")
 t = data.messung(1).datenreihe("t").werte
@@ -20,11 +24,11 @@ T = data.messung(1).datenreihe("&J_A11").werte
 p = data.messung(1).datenreihe("p_B1").werte
 
 print("Temperaturverteilung")
-print("m=" + str(np.average(T))+" ; s="+str(np.std(T))+", err="+str(np.std(T)/np.sqrt(len(T))))
+print("m=" + str(np.round(np.average(T),1))+" ; s="+str(np.round(np.std(T),1))+", err="+str(round_up(np.std(T)/np.sqrt(len(T)),1)))
 plt.hist(T,7, normed=True)
 x=np.arange(np.average(T)-0.25, np.average(T)+0.25, 0.001)
 plt.plot(x, gauss(x, np.average(T), np.std(T)))
-plt.title(u"Rauschmessung bei Raumtemperatur\n $\sigma$=" + str(np.std(T))+" ; $\mu$="+str(np.average(T)))
+plt.title(u"Rauschmessung bei Raumtemperatur\n $\sigma$=" + str(np.round(np.std(T),1))+"$^{\circ}C$, $\mu$="+str(np.round(np.average(T),1))+ "$^{\circ}C$, $\sigma_{\mu} = $"+str(round_up(np.std(T)/np.sqrt(len(T)),1))+"$^{\circ}C$")
 plt.xlabel(u"T/°C")
 plt.ylabel('Relatives Vorkommen')
 plt.savefig("Images/RauschmessungRT_T_histo.jpg")
@@ -45,7 +49,7 @@ print("m=" + str(np.average(p))+", s="+str(np.std(p))+", err="+str(np.std(p)/np.
 x=np.arange(min(p), max(p), 0.01)
 plt.hist(p, 3, normed=True)#, bins=np.arange(984, 986, 0.25))
 plt.plot(x, gauss(x, np.average(p), np.std(p)))
-plt.title(u"Rauschmessung Druck \n $\sigma$=" + str(np.std(p))+"; $\mu$="+str(np.average(p)))
+plt.title(u"Rauschmessung Druck \n $\sigma$=" + str(np.round(np.std(p),2))+" hPa, $\mu$="+str(np.round(np.average(p),2))+" hPa, $\sigma_{\mu} = $" +str(round_up(np.std(p)/np.sqrt(len(p)),2))+" hPa")
 plt.xlabel(u"Druck/hPa")
 plt.ylabel('Relatives Vorkommen')
 plt.savefig("Images/RauschmessungRT_p_histo.jpg")
