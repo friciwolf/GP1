ff970db7dd91698ee4486ee7905fa0a2729bea3b
lc316353 <44553345+lc316353@users.noreply.github.com>
2018-11-05 22:44:17 +0100

    Update Rauschmessung.py

--- Dampfdruck/Rauschmessung.py
+++ Dampfdruck/Rauschmessung.py
@@ -21,26 +21,35 @@ def round_good(m,s,err): #für obere Abschätzung der Fehler
     err=std/sqrt(len)
     '''
     i=2
-    test=list(str(err))
+    test=list('{:.20f}'.format(err))
     while test[i]=='0':
         i+=1
     return np.round([m,s,err],i)
 
 plt.close('all')
 
-t, p, T = [], [], [] #Temperatur in °C!
+t, p, T = [], [], [] #Temperatur in °C, Druck in hPa!
 data = cassy.CassyDaten("Rauschmessung.lab")
 t = data.messung(1).datenreihe("t").werte
 T = data.messung(1).datenreihe("&J_A11").werte
 p = data.messung(1).datenreihe("p_B1").werte
 
-print("Temperaturverteilung")
+#----------------------------#
+#Auswertung der Rauschmessung#
+#----------------------------#
+
+#Mittelwerte, Standardabweichungen und Fehler
 mT,sT,errT=round_good(np.average(T),np.std(T),np.std(T)/np.sqrt(len(T)))
 mp,sp,errp=round_good(np.average(p),np.std(p),np.std(p)/np.sqrt(len(p)))
 
-#Zum Verhindern des Abspielens beim Importieren in Kalib.py
 if __name__ == "__main__": 
+    
+#------------------------------------------------------------
+#Umgebungstemperatur
+    print("Temperaturverteilung")
     print('m='+str(mT)+u' °C, std='+str(sT)+u' °C, err='+str(errT)+u' °C')
+    
+    #Histogramm
     plt.hist(T,7, normed=True)
     x=np.arange(np.average(T)-0.25, np.average(T)+0.25, 0.001)
     plt.plot(x, gauss(x, np.average(T), np.std(T)))
@@ -50,6 +59,7 @@ if __name__ == "__main__":
     plt.savefig("Images/RauschmessungRT_T_histo.jpg")
     plt.figure()
     
+    #Datenplot
     plt.plot(t, T)
     plt.title(u"Rauschmessung bei Raumtemperatur")
     plt.ylabel(u"T/°C")
@@ -57,11 +67,14 @@ if __name__ == "__main__":
     plt.plot(t,np.array([np.mean(T)]*len(t)),color='green')
     plt.savefig("Images/RauschmessungRT_T.jpg")
     plt.figure()
-    
+
+#------------------------------------------------------------
+#Umgebungsdruck
     print("==="*20)
     print("Druckverteilung")
     print('m='+str(mp)+' hPa, std='+str(sp)+' hPa, err='+str(errp)+' hPa')
     
+    #Histogramm
     x=np.arange(min(p), max(p), 0.01)
     plt.hist(p, 3, normed=True)#, bins=np.arange(984, 986, 0.25))
     plt.plot(x, gauss(x, np.average(p), np.std(p)))
@@ -71,6 +84,7 @@ if __name__ == "__main__":
     plt.savefig("Images/RauschmessungRT_p_histo.jpg")
     plt.figure()
     
+    #Datenplot
     plt.plot(t, p)
     plt.ylabel(u"Druck/hPa")
     plt.xlabel('Zeit/s')
