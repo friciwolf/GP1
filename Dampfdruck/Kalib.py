b668ae2789c0ff998ff596d63925d6d1e2d7148a
lc316353 <44553345+lc316353@users.noreply.github.com>
2018-11-06 08:38:57 +0100

    Update Kalib.py

--- Dampfdruck/Kalib.py
+++ Dampfdruck/Kalib.py
@@ -3,7 +3,7 @@
 """
 Created on Sat Oct 27 19:27:20 2018
 
-@author: Mate
+@author: Mate, Patrick
 """
 
 """
@@ -34,11 +34,12 @@ sigma_p_dig=1500*0.05/100 #Auflösung Druck
 def gauss(x, m, s):
     return(np.power(scipy.pi*2*s**2, -0.5)*np.exp(-(x-m)**2/(2*s**2)))
 
-def T_th(T_gem, a, b, sT_gem, sa, sb):
+def T_th(T_gem, a, b, sT_gem, sa, sb,sT_gemsys,sasys,sbsys):
     T = (T_gem-b-50)/a + 50
     #Gauss auf T_th = (T_gem-b-50)/a + 50) bzgl. a, b, T_gem:
-    sT = np.sqrt((T_gem/a**2)**2 * sa**2 + a**-2 * sb**2 + a**-2 * sT_gem**2)
-    return np.array([T, sT])
+    sT = np.sqrt(((T_gem-b-50)/a**2)**2 * sa**2 + a**-2 * sb**2 + a**-2 * sT_gem**2)
+    sTsys = np.sqrt(((T_gem-b-50)/a**2)**2 * sasys**2 + a**-2 * sbsys**2 + a**-2 * sT_gemsys**2)
+    return np.array([T, sT, sTsys])
 
 #Erste Messung
 t, p, T = [], [], [] #Temperatur in °C!
@@ -93,9 +94,9 @@ print("\nTemperaturkalibrierung bei 0°C")
 
 #Erstauswertung
 mT0,sT0,errT0=Rauschmessung.round_good(np.mean(T),np.std(T),np.std(T)/np.sqrt(len(T)))
-errstatT0=Rauschmessung.sT/np.sqrt(len(T))
+errstatT0=Rauschmessung.round_good(0,0,Rauschmessung.sT/np.sqrt(len(T)))[2]
 errsysT0=sigmaT_Gerät_kl_70
-print("T0=" + str(mT0)+" °C, s="+str(sT0)+" °C,err="+str(errT0)+" °C, errstat="+str(errstatT0)+' °C')
+print("T0=" + str(mT0)+" °C, s="+str(sT0)+" °C,err="+str(errT0)+" °C, errstat="+str(errstatT0)+' °C\n')
 
 #Auftragen der Daten
 plt.title("Temperatur bei T=0°C\n $\sigma_{\mu}$=" + str(errT0)+"°C, $\mu$="+str(mT0)+"°C (ab t=200 s berechnet)")
@@ -108,7 +109,7 @@ plt.figure()
 
 #Histogramm
 x=np.arange(np.average(T)-0.4, np.average(T)+0.4, 0.01)
-plt.hist(T, normed=True)
+plt.hist(T,7, normed=True)
 plt.plot(x, gauss(x, np.average(T), np.std(T)))
 plt.title("Streuung der gemessenen Temperaturwerte bei T=0°C\n $\sigma_{\mu}$=" + str(errT0)+"°C, $\mu$="+str(mT0)+"°C (ab t=200 s berechnet)")
 plt.ylabel('Relatives Vorkommen')
@@ -134,7 +135,7 @@ p_sliced = p[240:1500]
 #Erstauswertung
 mT100,sT100,errT100=Rauschmessung.round_good(np.mean(T_sliced),np.std(T_sliced),np.std(T_sliced)/np.sqrt(len(T_sliced)))
 mp100,sp100,errp100=Rauschmessung.round_good(np.mean(p_sliced),np.std(p_sliced),np.std(p_sliced)/np.sqrt(len(p_sliced)))
-errstatT100=Rauschmessung.sT/np.sqrt(len(T_sliced))
+errstatT100=Rauschmessung.round_good(0,0,Rauschmessung.sT/np.sqrt(len(T_sliced)))[2]
 errsysT100=sigmaT_Gerät_gr_70
 print("\nTemperaturkalibrierung bei 100°C")
 print("T100=" + str(mT100)+" °C, s="+str(sT100)+" °C,err="+str(errT100)+" °C, errstat="+str(errstatT100)+' °C\n')
@@ -168,7 +169,7 @@ plt.figure()
 
 #Temperaturhistogramm
 x = np.arange(mT100-0.75,mT100+0.75, 0.01)
-plt.hist(T, normed=True)
+plt.hist(T,9, normed=True)
 plt.plot(x, gauss(x, mT100, np.std(T_sliced)))
 plt.gcf().subplots_adjust(top=0.8)
 plt.title("Streuung der gemessenen Temperaturwerte bei T=100°C\n $\mu$ ="+ str(mT100)+" °C, " + "$\sigma_{\mu}$ = " + str(errT100) + " °C \n (im Intervall t $\in$ [24 s; 150 s])")
@@ -179,28 +180,26 @@ plt.savefig("Images/Kalib_T_histo_100.pdf")
 #------------------------------------------------------------
 #Kalibrierung
 print("\n\nErgebnis auf die Korrekturgleichung:")
-#Die Fehler kommen aus der Rauschmessung, aus der Fluktuation der Werte bei Temperatur T und aus dem Fehler des Geräts:
-#sigmaT_tot**2 = sigmaT_Rausch**2 + sigmaT_Gerät**2 + sigmaT0**2
-
-#Ab hier nochmal überprüfen
-
-sigmaT0_tot = np.sqrt(sigmaT_Rausch**2+sigmaT_Gerät_kl_70**2+sigmaT0**2)
-sigmaT100_tot = np.sqrt(sigmaT_Rausch**2+sigmaT_Gerät_gr_70**2+sigmaT100**2)
-print("T bei T=0°C : T_gem = " + str(np.round(mT0,1))+" °C +-"+str(sigmaT0_tot) + " °C")
-print("T bei T=100°C : T_gem = " + str(np.round(mT100,1))+" °C +-"+str(sigmaT100_tot) + " °C")
-      
+print("T bei T=0°C : T_gem = " + str(mT0)+" °C +-"+str(errT0) + " °C +-"+str(errsysT0)+' °C')
+print("T bei T=100°C : T_gem = " + str(mT100)+" °C +-"+str(errT100) + " °C +-"+str(errsysT100)+' °C')
+#Der statistische Fehler aus der Rauschmessung und der systematische des Geräts werden einzeln fortgepflanzt
+    
 #Führe lineare Reg. bzgl. T_gem durch, dann stelle auf T_th um, weil wir bei der lin_reg nur Fehler auf y angeben können...
-a,ea,b,eb,chiq,corr = ana.lineare_regression(np.array([0, 100])-50,np.array([mT0, mT100])-50, np.array([sigmaT0_tot,sigmaT100_tot]))
+a,ea,b,eb,chiq,corr = ana.lineare_regression(np.array([0, 100])-50,np.array([mT0, mT100])-50, np.array([Rauschmessung.sT,Rauschmessung.sT])) 
+asys,easys,bsys,ebsys,chiq,corr = ana.lineare_regression(np.array([0, 100])-50,np.array([mT0, mT100])-50, np.array([errsysT0,errsysT100]))
+print("\nErgebnis des linearen Modells:") #asys=a; bsys=b
+a,ea,easys=Rauschmessung.round_good(a,ea,easys)
+b,eb,ebsys=Rauschmessung.round_good(b,eb,ebsys)
+print('a = (%g +- %g +- %g) hPa/s,   b = (%g +- %g +- %g) hPa' % (a, ea,easys, b, eb,ebsys))
 print("\nDamit ergibt sich die Korrekturgleichung:")
-print("T_gem = "+ str(a)+"*(T_th-50) + " + str(b)+" + 50")
-print("oder auf T_th umgestellt:")
-print("T_th = (T_gem-b-50)/a + 50")
-print("mit a = " + str(a) + " C^-1 +- " + str(ea) + " °C^-1")
+print("T_gem = "+ str(a)+"*(T_th-50 °C) + " + str(b)+"°C + 50°C")
+print("oder nach T_th umgestellt:")
+print("T_th = (T_gem-b-50°C)/a + 50°C")
+print("mit a = " + str(a) + " " + str(ea))
 print("und mit b = " + str(b) + " °C +- " + str(eb) + " °C")
 
-T_gem=99.8
-T_theo, sT = T_th(T_gem,a,b,sigmaT_Gerät_gr_70,ea,eb)
-print("damit haben wir T_th(99.8°C) = " + str (np.round(T_theo,1)) + " °C +-" + str(sT) + " °C")
-T_gem=2.5
-T_theo, sT = T_th(T_gem,a,b,sigmaT_Gerät_kl_70,ea,eb)
-print("und T_th(2.5°C) = " + str (np.round(T_theo,1)) + " °C +-" + str(sT) + " °C")
+#Prüfung
+T_theo, sT, sTsys= Rauschmessung.round_good(*T_th(mT100,a,b,Rauschmessung.sT,ea,eb,sigmaT_Gerät_gr_70,easys,ebsys))
+print("\nSomit haben wir T_th(T100) = " + str (T_theo) + " °C +-" + str(sT) + " °C +-" + str(sTsys) + " °C")
+T_theo, sT, sTsys= Rauschmessung.round_good(*T_th(mT0,a,b,Rauschmessung.sT,ea,eb,sigmaT_Gerät_kl_70,easys,ebsys))
+print("und T_th(T0) = " + str (T_theo) + " °C +-" + str(sT) + " °C +-" + str(sTsys) + " °C")
