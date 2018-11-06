9400dd10853d45689172400bf12b17e3e600dc7c
lc316353 <44553345+lc316353@users.noreply.github.com>
2018-11-06 19:39:25 +0100

    Update Dampfdruck.py

--- Dampfdruck/Dampfdruck.py
+++ Dampfdruck/Dampfdruck.py
@@ -22,9 +22,10 @@ import scipy.constants as sc
 from praktikum import analyse as ana
 import praktikum.cassy as cassy
 import Kalib
+import Rauschmessung
 
-def T_real(T_gem):
-    return  (T_gem-Kalib.b-50)/Kalib.a +50
+def T_th(T_gem):
+    return (T_gem-Kalib.b-50)/Kalib.a +50
     
 #Fehler Beachten+Daraufaddieren!!!!!
 R=sc.R
@@ -36,18 +37,26 @@ t = data.messung(1).datenreihe("t").werte
 T = data.messung(1).datenreihe("&J_A11").werte
 p = data.messung(1).datenreihe("p_B1").werte
 
-#Mitbetrachtung der Kalibrierung:
-T = T_real(T) #TODO: Fehler
-T = T+273.15
-#Plots
 
-plt.plot(t, T, color='red')
+#-----------------------------------------------------------------------#
+#Auswertung der Dampfdruckkurve und Bestimmung der Verdampfungsenthalpie#
+#-----------------------------------------------------------------------#
+
+#------------------------------------------------------------
+#Ausführen der Kalibrierung
+T_real,sTstat,sTsys=Kalib.T_th(T,Kalib.a,Kalib.b,Rauschmessung.sT,Kalib.ea,Kalib.eb,Kalib.sigmaT_Gerät_gr_70,Kalib.easys,Kalib.ebsys)
+T_real = T_real+273.15 #zu Kelvin
+#TODO: Fehler
+
+#Plot T
+plt.plot(t, T_real, color='red')
 plt.title("Abkühlung des Kolbens")
 plt.ylabel("Temperatur in K")
 plt.xlabel("Zeit in s")
 plt.savefig("Images/Dampfdruck_T.pdf")
 plt.figure()
 
+#Plot p
 plt.plot(t, p)
 plt.title("Dampfdruckänderung im Kolben")
 plt.ylabel("Druck in hPa")
@@ -55,17 +64,23 @@ plt.xlabel("Zeit in s")
 plt.savefig("Images/Dampfdruck_p.pdf")
 plt.figure()
 
-L_Werte=[[],[]]
-T_Werte=[[],[]]
+#------------------------------------------------------------
+#
+L_Werte=[[],[],[]]
+T_Werte=[[],[],[]]
 for i in range(1,11):
-    xi=np.power(T[:1000*i], -1)-np.power(T[0], -1)
+    xi=np.power(T_real[:1000*i], -1)-np.power(T_real[0], -1)
     yi=(-np.log(p)[:1000*i]+np.log(p[0]))*R
     L,eL,b,eb,chiq,corr = ana.lineare_regression_xy(xi, yi, np.ones(len(xi)), np.ones(len(yi)))
+    Lsys,eLsys,bsys,ebsys,chiq,corr = ana.lineare_regression_xy(xi, yi, np.ones(len(xi)), np.ones(len(yi)))
     L_Werte[0].append(L)
     L_Werte[1].append(eL)
-    T_Werte[0].append(T[1000*i])
-    T_Werte[1].append(1) #TODO: Fehler
+    L_Werte[2].append(eLsys)
+    T_Werte[0].append(T_real[1000*i])
+    T_Werte[1].append(sTstat[1000*i])
+    T_Werte[2].append(sTsys[1000*i])#TODO: Fehler
 
+#Plot
 L,eL,b,eb,chiq,corr = ana.lineare_regression_xy(np.array(T_Werte[0]), np.array(L_Werte[0]), np.array(T_Werte[1]), np.array(L_Werte[1]))
 plt.plot(T_Werte[0], L_Werte[0])
 x=np.arange(min(T_Werte[0])-2,max(T_Werte[0])+2,0.01)
@@ -73,13 +88,17 @@ plt.plot(x, L*x+b)
 plt.title("Verdampfungsenthalpie in Abhängigkeit von T")
 plt.xlabel('Temperatur in K')
 plt.ylabel('Enthalpie in J/mol')
+plt.savefig('Images/Dampfdruck_')
 plt.figure()
+
+#Wertetabelle
 print("\nTabelle von Werten")
 print("    T     ||        L   ")
 for i in range(1,11):
     print('{:.5f} || {}'.format(T_Werte[0][i-1],L*T_Werte[0][i-1]+b))
 
-x=np.power(T[:N], -1)-np.power(T[0], -1)
+#
+x=np.power(T_real[:N], -1)-np.power(T_real[0], -1)
 y=(-np.log(p)[:N]+np.log(p[0]))*R
 plt.plot(x, y)
 plt.xlabel("1/T-1/T_0")
@@ -87,6 +106,6 @@ plt.ylabel("-R*ln(p/p_0)")
 
 L,eL,b,eb,chiq,corr = ana.lineare_regression_xy(x, y, np.ones(len(x)), np.ones(len(y)))
 plt.plot(x, L*x+b, color='green')
-plt.title(str(T[N]))
+plt.title(str(T_real[N]))
 plt.show()
 print('\nL = (%g +- %g) J/mol,   b = (%g +- %g) J/(mol*K),  chi2/dof = %g / %g  corr = %g' % (L, eL, b, eb, chiq, len(x)-2, corr))
