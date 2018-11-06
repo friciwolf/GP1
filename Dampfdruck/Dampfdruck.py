5926deb300e0f08d009cdbf1339fc5b577ab35a2
Mate <friciwolf@gmail.com>
2018-11-06 21:09:17 +0100

    Update Dampfdruck/Dampfdruck.py

--- Dampfdruck/Dampfdruck.py
+++ Dampfdruck/Dampfdruck.py
@@ -65,30 +65,39 @@ plt.savefig("Images/Dampfdruck_p.pdf")
 plt.figure()
 
 #------------------------------------------------------------
-#
-L_Werte=[[],[],[]]
+#Berechnung der Verdampfungsenthalpie
+L_Werte=[[],[],[], []] #L_sys, s_sys, L_stat, s_stat
 T_Werte=[[],[],[]]
 for i in range(1,11):
     xi=np.power(T_real[:1000*i], -1)-np.power(T_real[0], -1)
     yi=(-np.log(p)[:1000*i]+np.log(p[0]))*R
-    L,eL,b,eb,chiq,corr = ana.lineare_regression_xy(xi, yi, np.ones(len(xi)), np.ones(len(yi)))
-    Lsys,eLsys,bsys,ebsys,chiq,corr = ana.lineare_regression_xy(xi, yi, np.ones(len(xi)), np.ones(len(yi)))
-    L_Werte[0].append(L)
-    L_Werte[1].append(eL)
-    L_Werte[2].append(eLsys)
+    sxi_sys = np.sqrt(np.power(T_real[:1000*i], -4)*sTsys[:1000*i]**2+ np.power(T_real[0], -4)*sTsys[0])
+    syi_sys = R*Kalib.sigmap*np.sqrt(np.power(p[:1000*i], -2)+np.power(p[0], -2))
+    Lsys,eLsys,bsys,ebsys,chiqsys,corr = ana.lineare_regression_xy(xi, yi, sxi_sys, syi_sys)
+    sxi_stat = np.sqrt(np.power(T_real[:1000*i], -4)*sTstat[:1000*i]**2+ np.power(T_real[0], -4)*sTstat[0])
+    syi_stat = R*Kalib.sigmap*np.sqrt(np.power(p[:1000*i], -2)+np.power(p[0], -2))
+    Lstat,eLstat,bstat,ebstat,chiqstat,corr = ana.lineare_regression_xy(xi, yi, sxi_stat, syi_stat)
+    L_Werte[0].append(Lsys)
+    L_Werte[1].append(eLsys)
+    L_Werte[2].append(Lstat)
+    L_Werte[3].append(eLstat)
     T_Werte[0].append(T_real[1000*i])
     T_Werte[1].append(sTstat[1000*i])
-    T_Werte[2].append(sTsys[1000*i])#TODO: Fehler
+    T_Werte[2].append(sTsys[1000*i])
 
-#Plot
-L,eL,b,eb,chiq,corr = ana.lineare_regression_xy(np.array(T_Werte[0]), np.array(L_Werte[0]), np.array(T_Werte[1]), np.array(L_Werte[1]))
+#wichte L nach den Fehlern
+L_gew = (L_Werte[0]/np.power(L_Werte[1], 2) + L_Werte[2]/np.power(L_Werte[3], 2))/(np.power(L_Werte[1], -2) + np.power(L_Werte[3], -2))
+
+#Plot der Verdampfungsenthalpie
+L,eL,b,eb,chiq,corr = ana.lineare_regression_xy(np.array(T_Werte[0]), np.array(L_gew), np.array(T_Werte[1]), np.array(L_Werte[1]))
 plt.plot(T_Werte[0], L_Werte[0])
+plt.errorbar(T_Werte[0], L_gew, L_Werte[1], T_Werte[2], ecolor="red", fmt='or', markersize=4, capsize=3)
 x=np.arange(min(T_Werte[0])-2,max(T_Werte[0])+2,0.01)
 plt.plot(x, L*x+b)
 plt.title("Verdampfungsenthalpie in Abhängigkeit von T")
 plt.xlabel('Temperatur in K')
 plt.ylabel('Enthalpie in J/mol')
-plt.savefig('Images/Dampfdruck_')
+plt.savefig('Images/Dampfdruck_L.pdf')
 plt.figure()
 
 #Wertetabelle
@@ -97,15 +106,18 @@ print("    T     ||        L   ")
 for i in range(1,11):
     print('{:.5f} || {}'.format(T_Werte[0][i-1],L*T_Werte[0][i-1]+b))
 
-#
+#Für die Clausius-Clapeyron-Ausgleichsgerade
 x=np.power(T_real[:N], -1)-np.power(T_real[0], -1)
+sx = np.sqrt(np.power(T_real[:N], -4)*sTsys[:N]**2+ np.power(T_real[0], -4)*sTsys[0])
 y=(-np.log(p)[:N]+np.log(p[0]))*R
+sy = R*Kalib.sigmap*np.sqrt(np.power(p[:N], -2)+np.power(p[0], -2))
 plt.plot(x, y)
-plt.xlabel("1/T-1/T_0")
-plt.ylabel("-R*ln(p/p_0)")
+plt.xlabel("$1/T-1/T_0$")
+plt.ylabel("$-R*\ln(p/p_0)$")
 
-L,eL,b,eb,chiq,corr = ana.lineare_regression_xy(x, y, np.ones(len(x)), np.ones(len(y)))
+L,eL,b,eb,chiq,corr = ana.lineare_regression_xy(x, y, sx, sy)
 plt.plot(x, L*x+b, color='green')
-plt.title(str(T_real[N]))
+plt.title("Clausius-Clapeyron-Ausgleichsgerade")
 plt.show()
+plt.savefig("Images/Dampfdruck_ln.pdf")
 print('\nL = (%g +- %g) J/mol,   b = (%g +- %g) J/(mol*K),  chi2/dof = %g / %g  corr = %g' % (L, eL, b, eb, chiq, len(x)-2, corr))
