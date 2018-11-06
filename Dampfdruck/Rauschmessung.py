f2f063fe6332a2854c4e9559c22ab9aa92da4625
Mate <friciwolf@gmail.com>
2018-11-06 17:03:22 +0100

    Update Dampfdruck/Rauschmessung.py

--- Dampfdruck/Rauschmessung.py
+++ Dampfdruck/Rauschmessung.py
@@ -24,11 +24,11 @@ T = data.messung(1).datenreihe("&J_A11").werte
 p = data.messung(1).datenreihe("p_B1").werte
 
 print("Temperaturverteilung")
-print("m=" + str(np.round(np.average(T),1))+" °C , s="+str(np.round(np.std(T),1))+" °C , err="+str(round_up(np.std(T)/np.sqrt(len(T)),1))+" °C")
+print("m=" + str(np.round(np.average(T),1))+" °C , s="+str(np.round(np.std(T),2))+" °C , err="+str(round_up(np.std(T)/np.sqrt(len(T)),1))+" °C")
 plt.hist(T,7, normed=True)
 x=np.arange(np.average(T)-0.25, np.average(T)+0.25, 0.001)
 plt.plot(x, gauss(x, np.average(T), np.std(T)))
-plt.title(u"Rauschmessung bei Raumtemperatur\n $\sigma$=" + str(np.round(np.std(T),1))+"$^{\circ}C$, $\mu$="+str(np.round(np.average(T),1))+ "$^{\circ}C$, $\sigma_{\mu} = $"+str(round_up(np.std(T)/np.sqrt(len(T)),1))+"$^{\circ}C$")
+plt.title(u"Rauschmessung bei Raumtemperatur\n $\sigma$=" + str(np.round(np.std(T),2))+"$^{\circ}C$, $\mu$="+str(np.round(np.average(T),1))+ "$^{\circ}C$, $\sigma_{\mu} = $"+str(round_up(np.std(T)/np.sqrt(len(T)),1))+"$^{\circ}C$")
 plt.xlabel(u"T/°C")
 plt.ylabel('Relatives Vorkommen')
 plt.savefig("Images/RauschmessungRT_T_histo.jpg")
