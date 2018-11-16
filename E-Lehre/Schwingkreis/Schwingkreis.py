deccd4a50ae2ca41b650de4d5d1db74ed7cbd38f
Mate <friciwolf@gmail.com>
2018-11-17 00:34:02 +0100

    Update Schwingkreis.py

--- E-Lehre/Schwingkreis/Schwingkreis.py
+++ E-Lehre/Schwingkreis/Schwingkreis.py
@@ -32,30 +32,66 @@ def c_open(file):
     returns: t, U, I 
     """
     data = cassy.CassyDaten(file)
-    t = data.messung(1).datenreihe("t").werte, 
+    t = data.messung(1).datenreihe("t").werte
     I = data.messung(1).datenreihe("I_A2").werte
-    U = data.messung(1).datenreihe("U_B2").werte, 
+    U = data.messung(1).datenreihe("U_B2").werte 
     return t, U, I
 
-t1,t2, U1, U2 = [], [], [], []
-t1, U1 = f_open("ALL0000/U1.dat")
-t2, U2 = f_open("ALL0000/U2.dat")
+def lok_max(t, U):
+    """
+    Berechnet die lokalen Maxima einer Schwingung
+    """
+    Tmax, m = [], []
+    for i in range(10, len(U)-11):
+        if max(np.array(U[i-10:i+10])) == U[i]:
+            Tmax.append(t[i])
+            m.append(U[i])
+    return Tmax, m
+#TODO: Entscheidung ob Einhüllende Sinn fürs Plot macht
+def delta(t, U):
+    """
+    Berechnet das Delta einer Schwingung
+    """
+    delta = 0.0
+    tmaxima, maxima = lok_max(t, U)
+    delta = np.log(maxima[1]/maxima[0])/(tmaxima[1]-tmaxima[0])
+    return delta
 
-plt.title("Entladung des Kondensators über den Schwingkreis \n $R = 10 \Omega$")
-plt.plot(t1, U1, label="U1")
-plt.plot(t2, np.array(U2)*10, label="U2 - scale factor: 10")
-plt.legend()
-plt.show()
+#Plot der Messungen mit dem Oszilloskop
+for i in range(4):
+    t1,t2, U1, U2 = [], [], [], []
+    t1, U1 = f_open("ALL000"+str(i)+"/U1.dat")
+    t2, U2 = f_open("ALL000"+str(i)+"/U2.dat")
+    plt.title("Entladung des Kondensators über den Schwingkreis \n R = ??? $\Omega$")
+    plt.plot(t1, U1, label="U1")
+    plt.plot(t2, U2, label="U2")
+    plt.savefig("Images/O_"+str(i)+".pdf")
+    plt.legend()
+    plt.show()
+    plt.close()
 
+#Plot der Messungen mit den Widerständen R:
 R = [5.1, 47, 100, 1000, 208, 173, 137, 100.6]
-
 for i in range(len(R)): 
     f = str(R[i])+"_Messung_"+str(i+1)+".lab"
     t, U, I = c_open(f)
     plt.title("Entladung des Kondensators über den Schwingkreis \n R = " +str(R[i]) + " $\Omega$")
-    plt.plot(t[0], I, label="I")
-    plt.plot(t[0], U[0], label="U")
-    plt.savefig("Images/"+str(R[i])+".pdf")
+    d = delta(t, U)
+    plt.plot(t, U[0]*np.exp(d*np.array(t)), label = "Einhüllende")
+    plt.plot(t, I, label="I")
+    plt.plot(t, U, label="U")
+    plt.savefig("Images/C_"+str(R[i])+".pdf")
     plt.legend()
     plt.show()
     plt.close()
+
+#Grenzfall:
+f = "Grenzfall_Messung_9.lab"
+t, U, I = c_open(f)
+plt.title("Entladung des Kondensators über den Schwingkreis - Grenzfall bei \n R = ??? $\Omega$")
+plt.plot(t, I, label="I")
+plt.plot(t, U, label="U")
+plt.savefig("Images/grenzfall.pdf")
+plt.legend()
+plt.show()
+plt.close()
