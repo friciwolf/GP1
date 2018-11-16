d8a379cad6956309013818cc034200b76a81c0bf
Mate <friciwolf@gmail.com>
2018-11-16 10:14:45 +0100

    Update Schwingkreis.py

--- E-Lehre/Schwingkreis/Schwingkreis.py
+++ E-Lehre/Schwingkreis/Schwingkreis.py
@@ -36,7 +36,7 @@ def c_open(file):
     I = data.messung(1).datenreihe("I_A2").werte
     U = data.messung(1).datenreihe("U_B2").werte, 
     return t, U, I
-#Hier kommt noch eine Schleife zur Erstellung der Plots...
+
 t1,t2, U1, U2 = [], [], [], []
 t1, U1 = f_open("ALL0000/U1.dat")
 t2, U2 = f_open("ALL0000/U2.dat")
@@ -47,10 +47,15 @@ plt.plot(t2, np.array(U2)*10, label="U2 - scale factor: 10")
 plt.legend()
 plt.show()
 
-f = "100_Messung_3.lab"
-t, U, I = c_open(f)
-plt.title("Entladung des Kondensators über den Schwingkreis \n R = " + f.split("_")[0]+ " $\Omega$")
-plt.plot(t[0], I, label="I")
-plt.plot(t[0], U[0], label="U")
-plt.legend()
-plt.show()
+R = [5.1, 47, 100, 1000, 208, 173, 137, 100.6]
+
+for i in range(len(R)): 
+    f = str(R[i])+"_Messung_"+str(i+1)+".lab"
+    t, U, I = c_open(f)
+    plt.title("Entladung des Kondensators über den Schwingkreis \n R = " +str(R[i]) + " $\Omega$")
+    plt.plot(t[0], I, label="I")
+    plt.plot(t[0], U[0], label="U")
+    plt.savefig("Images/"+str(R[i])+".pdf")
+    plt.legend()
+    plt.show()
+    plt.close()
