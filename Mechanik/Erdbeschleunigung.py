13d5ef892136fe589f17489349ed29e266920fcc
lc316353 <44553345+lc316353@users.noreply.github.com>
2018-11-30 22:49:46 +0100

    Add files via upload

---
+++ Mechanik/Erdbeschleunigung.py
@@ -0,0 +1,62 @@
+# -*- coding: utf-8 -*-
+
+import Rauschmessung
+
+offsetM=Rauschmessung.mM
+offsetSt=Rauschmessung.mSt
+
+l=0.68
+r=0.08035
+errl_stat=0.001
+errr_stat=0.0001
+errl_sys=0.0002+0.0003*1 # Güteklasse II:a+b*L mit a=0.2mm, b=0.3mm/m, L= auf ganzen Meter gerundete zu messende Länge
+T=[]
+errT=[]
+
+for i in range(1,4):
+    data = cassy.CassyDaten("Erdbeschleunigung/perfekte_anpassung_messung_"+str(i)+".lab")
+    t = data.messung(1).datenreihe("t").werte
+    M = data.messung(1).datenreihe("U_A1").werte-offsetM #Pendelkörper (Masse)
+    St = data.messung(1).datenreihe("U_B1").werte-offsetSt #Stange allein
+
+    #Rohdatenplot bis Stange zu gedämpft
+    bis=np.argmax(t>100)
+    plt.plot(t[:bis],M[:bis], color='black')
+    plt.plot(t[:bis],St[:bis],color='red')
+    plt.title('Schwingung mit körperloser Frequenz {}'.format(i))
+    plt.grid()
+    plt.savefig('Images/Erdbeschleunigung_Roh_'+str(i)+'.jpg')
+    if i!=3:
+        plt.figure()
+    
+    #Nullstellen zählen
+    count=0
+    for i,volt in enumerate(M):
+        if volt!=M[-1] and ((volt<=0 and M[i+1]>0) or (volt>=0 and M[i+1]<0)):
+            count+=1
+            ende=i
+            if count==1:
+                start=i
+    if count%2==1:
+        count-=1
+        
+    #Periodendauer berechnen
+    errt=(t[-1]-t[0])/len(t)
+    T.append(2*(t[ende]-t[start])/count)
+    errT.append(2*np.sqrt(2)*errt/count) #TODO: Richtig?
+
+T,errT=analyse.gewichtetes_mittel(np.array(T),np.array(errT))
+
+#Frequenz und Erdbeschleunigung + Fehler
+f=1/T
+errf=f*errT/T
+print('Frequenz: f=({1} +- {2}) Hz'.format(*Rauschmessung.round_good(0,f,errf)))
+
+g=4*np.pi**2*f**2*l*(1+r**2/l**2/2)
+errg_stat=(g/l-4*np.pi**2*f**2*r**2/l**2)*errl_stat #TODO: vlt.genauer?
+errg_sys=(g/l-4*np.pi**2*f**2*r**2/l**2)*errl_sys
+if errg_sys>errg_stat: 
+   g,errg_stat,errg_sys=Rauschmessung.round_good(g,errg_stat,errg_sys)
+else: 
+   g,errg_sys,errg_stat=Rauschmessung.round_good(g,errg_sys,errg_stat)
+print('Erdbeschleunigung: g=({} +- {} +- {})m/s²'.format(g,errg_stat,errg_sys))
---
+++ Mechanik/Rauschmessung.py
@@ -0,0 +1,98 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Oct 27 17:24:44 2018
+
+@author: Mate, Patrick
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+import scipy
+import praktikum.cassy as cassy
+import praktikum.analyse as analyse
+
+def gauss(x, m, s):
+    return 1/np.sqrt(np.pi*2)/s*np.exp(-(x-m)**2/(2*s**2))
+
+def round_good(m,s,err): #für obere Abschätzung der Fehler
+    '''
+    Passt Mittelwert- und Standardabweichungsnachkommastellen an den Fehler an (2 signifikante Stellen des Fehlers)
+    m=mean
+    s=std
+    err=std/sqrt(len)
+    '''
+    i=2
+    test=list('{:.20f}'.format(err))
+    while test[i]=='0':
+        i+=1
+    return np.round([m,s,err],i)
+
+plt.close('all')
+
+
+data = cassy.CassyDaten("Erdbeschleunigung/Rauschmessung_umgepolt.lab")
+t = data.messung(1).datenreihe("t").werte
+M = data.messung(1).datenreihe("U_A1").werte #Pendelkörper (Masse)
+St = data.messung(1).datenreihe("U_B1").werte #Stange allein
+
+#----------------------------#
+#Auswertung der Rauschmessung#
+#----------------------------#
+
+#Mittelwerte, Standardabweichungen und Fehler
+mM,sM,errM=round_good(np.average(M),np.std(M),np.std(M)/np.sqrt(len(M)))
+mSt,sSt,errSt=round_good(np.average(St),np.std(St),np.std(St)/np.sqrt(len(St)))
+
+if __name__ == "__main__": 
+    
+#------------------------------------------------------------
+#Umgebungstemperatur
+    print("Temperaturverteilung")
+    print('m='+str(mM)+u' V, std='+str(sM)+u' V, err='+str(errM)+u' V')
+    
+    #Histogramm
+    weightsM=np.ones_like(M)/float(len(M))
+    plt.hist(M,7, normed=0, weights=weightsM)
+    x=np.arange(np.average(M)-0.03, np.average(M)+0.03, 0.001)
+    #plt.plot(x, gauss(x, np.average(M), np.std(M)))
+    plt.title(u"Histogramm der Ruhelage vom Pendel mit Körper\n $\sigma$=" + str(sM)+"$V$, $\mu$="+str(mM)+ "$V$, $\sigma_{\mu} = $"+str(errM)+"$V$")
+    plt.xlabel(u"U/V")
+    plt.ylabel('Relatives Vorkommen')
+    plt.savefig("Images/Rauschmessung_mitMasse_histo.jpg")
+    plt.axvline(np.mean(M),color='red',linestyle='--')
+    plt.figure()
+    
+    #Datenplot
+    plt.plot(t, M)
+    plt.title(u"Rauschmessung der Ruhelage des Pendels mit Körper")
+    plt.ylabel(u"T/°C")
+    plt.xlabel('Zeit/s')
+    plt.plot(t,np.array([np.mean(M)]*len(t)),color='darkorange')
+    plt.savefig("Images/Rauschmessung_mitMasse.jpg")
+    plt.figure()
+
+#------------------------------------------------------------
+#Umgebungsdruck
+    print("==="*20)
+    print("Druckverteilung")
+    print('m='+str(mSt)+' V, std='+str(sSt)+' V, err='+str(errSt)+' V')
+    
+    #Histogramm
+    weightsSt=np.ones_like(M)/float(len(M))
+    plt.hist(St, 7, normed=0,weights=weightsSt)
+    x=np.arange(min(St), max(St), 0.0001)
+    #plt.plot(x, gauss(x, np.average(St), np.std(St)))
+    plt.title(u"Histogramm der Ruhelage der Pendelstange \n $\sigma$=" + str(sSt)+" V, $\mu$="+str(mSt)+" V, $\sigma_{\mu} = $" +str(errSt)+" V")
+    plt.xlabel(u"U/V")
+    plt.ylabel('Relatives Vorkommen')
+    plt.savefig("Images/Rauschmessung_nurStange_histo.jpg")
+    plt.axvline(np.mean(St),color='red',linestyle='--')
+    plt.figure()
+    
+    #Datenplot
+    plt.plot(t, St)
+    plt.ylabel(u"U/V")
+    plt.xlabel('Zeit/s')
+    plt.title("Rauschmessung der Ruhelage der Pendelstange")
+    plt.plot(t,np.array([np.mean(St)]*len(t)),color='darkorange')
+    plt.savefig("Images/Rauschmessung_nurStange.jpg")
