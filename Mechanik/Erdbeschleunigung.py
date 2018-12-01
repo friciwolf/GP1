dc61afa0adcd07fe29a5e37875643bc2acc74208
lc316353 <44553345+lc316353@users.noreply.github.com>
2018-12-01 10:23:23 +0100

    Update Erdbeschleunigung.py

--- Mechanik/Erdbeschleunigung.py
+++ Mechanik/Erdbeschleunigung.py
@@ -6,10 +6,10 @@ offsetM=Rauschmessung.mM
 offsetSt=Rauschmessung.mSt
 
 l=0.68
-r=0.08035
+r=0.08035/2
 errl_stat=0.001
 errr_stat=0.0001
-errl_sys=0.0002+0.0003*1 # Güteklasse II:a+b*L mit a=0.2mm, b=0.3mm/m, L= auf ganzen Meter gerundete zu messende Länge
+errl_sys=0.0003+0.0002*1 # Güteklasse II:a+b*L mit a=0.3mm, b=0.2mm/m, L= auf ganzen Meter gerundete zu messende Länge
 T=[]
 errT=[]
 
@@ -18,16 +18,17 @@ for i in range(1,4):
     t = data.messung(1).datenreihe("t").werte
     M = data.messung(1).datenreihe("U_A1").werte-offsetM #Pendelkörper (Masse)
     St = data.messung(1).datenreihe("U_B1").werte-offsetSt #Stange allein
-
-    #Rohdatenplot bis Stange zu gedämpft
-    bis=np.argmax(t>100)
-    plt.plot(t[:bis],M[:bis], color='black')
-    plt.plot(t[:bis],St[:bis],color='red')
-    plt.title('Schwingung mit körperloser Frequenz {}'.format(i))
-    plt.grid()
-    plt.savefig('Images/Erdbeschleunigung_Roh_'+str(i)+'.jpg')
-    if i!=3:
-        plt.figure()
+    
+    if __name__=='__main__':
+        #Rohdatenplot bis Stange zu gedämpft
+        bis=np.argmax(t>100)
+        plt.plot(t[:bis],M[:bis], color='black')
+        plt.plot(t[:bis],St[:bis],color='red')
+        plt.title('Schwingung mit körperloser Frequenz {}'.format(i))
+        plt.grid()
+        plt.savefig('Images/Erdbeschleunigung_Roh_'+str(i)+'.jpg')
+        if i!=3:
+            plt.figure()
     
     #Nullstellen zählen
     count=0
@@ -50,10 +51,10 @@ T,errT=analyse.gewichtetes_mittel(np.array(T),np.array(errT))
 #Frequenz und Erdbeschleunigung + Fehler
 f=1/T
 errf=f*errT/T
-print('Frequenz: f=({1} +- {2}) Hz'.format(*Rauschmessung.round_good(0,f,errf)))
+print('\nFrequenz: f=({1} +- {2}) Hz'.format(*Rauschmessung.round_good(0,f,errf)))
 
 g=4*np.pi**2*f**2*l*(1+r**2/l**2/2)
-errg_stat=(g/l-4*np.pi**2*f**2*r**2/l**2)*errl_stat #TODO: vlt.genauer?
+errg_stat=np.sqrt(((g/l-4*np.pi**2*f**2*r**2/l**2)*errl_stat)**2+(2*g/f*errf)**2) #TODO: vlt.genauer?
 errg_sys=(g/l-4*np.pi**2*f**2*r**2/l**2)*errl_sys
 if errg_sys>errg_stat: 
    g,errg_stat,errg_sys=Rauschmessung.round_good(g,errg_stat,errg_sys)
